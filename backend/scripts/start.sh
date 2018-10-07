ae08a3694457965f5144f876ae7fb3d8b11264ac
Vitaliy Pavlenko <vivpavlenko@gmail.com>
2018-10-07 22:08:19 +0300

    added default user (admin)

--- backend/scripts/start.sh
+++ backend/scripts/start.sh
@@ -7,4 +7,5 @@ set -o xtrace
 
 python manage.py migrate
 python manage.py collectstatic --noinput --verbosity 0
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='$DEFAULT_EMAIL').exists() or  User.objects.create_superuser('$DEFAULT_EMAIL', '$DEFAULT_PASS')"
 python manage.py runserver_plus 0.0.0.0:8000
