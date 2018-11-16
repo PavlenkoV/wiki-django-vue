b7e13164f0f65698322992516b2a9d2ae38268b6
Vitaliy Pavlenko <vivpavlenko@gmail.com>
2018-11-16 14:01:30 +0200

    added author to wikipages

--- backend/apps/wikipages/models.py
+++ backend/apps/wikipages/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 
+from apps.users.models import User
+
 
 class WikiPage(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
+    author = models.ForeignKey(User, related_name='wikipages', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
